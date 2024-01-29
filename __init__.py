c2d2efb325996628679271f6c3bb520aa4d29833
Volodymyr Zakalyk <volodymyr.zakalyk@gmail.com>
2024-01-29 14:07:39 +0200

    __init__: Add identifiers (#253)

---
+++ __init__.py
@@ -0,0 +1,38 @@
+class Arg:
+    ...
+
+
+class App:
+    ...
+
+
+class ArgHelper:
+    ...
+
+
+class AppHelper:
+    ...
+
+
+class Completer:
+    ...
+
+
+class CompleterNone:
+    ...
+
+
+class CompleterList:
+    ...
+
+
+class CompleterPath:
+    ...
+
+
+class CallError:
+    ...
+
+
+def main():
+    ...
--- __init__.pyi
+++ __init__.pyi
@@ -3,3 +3,53 @@ Wrapper for argparse and argcomplete.
 
 Compatible with Python versions 3.6 - 3.11.
 '''
+
+
+class Arg:
+    '''
+    '''
+
+
+class App:
+    '''
+    '''
+
+
+class ArgHelper:
+    '''
+    '''
+
+
+class AppHelper:
+    '''
+    '''
+
+
+class Completer:
+    '''
+    '''
+
+
+class CompleterNone:
+    '''
+    '''
+
+
+class CompleterList:
+    '''
+    '''
+
+
+class CompleterPath:
+    '''
+    '''
+
+
+class CallError:
+    '''
+    '''
+
+
+def main():
+    '''
+    '''
